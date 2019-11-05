c2dc91dabd246c3ec4f8df86dc83cb917c33727d
Tomputer <tomek.gromadzki.sk@gmail.com>
2019-11-05 18:01:52 +0100

    Refactoring

--- src/main/java/pl/gromadzki/spittr/DemoData/DemoData.java
+++ src/main/java/pl/gromadzki/spittr/DemoData/DemoData.java
@@ -5,7 +5,6 @@ import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import pl.gromadzki.spittr.model.Spittle;
 import pl.gromadzki.spittr.repository.SpittleRepository;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -22,6 +21,7 @@ public class DemoData {
         repository.save(new Spittle("Testowa wkafsghasjkdfhas jkdhfags", "11-09-19 08:20:11", "username"));
         repository.save(new Spittle("Testowa wiadomosc 2", "01-10-19 19:32:11", "username"));
         repository.save(new Spittle("Testowa wiadomosc 3", simpleDateFormat.format(new Date()), "username"));
+
         //repository.findAll().forEach(System.out::println);
     }
 }
--- src/main/java/pl/gromadzki/spittr/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.gromadzki.spittr.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pl.gromadzki.spittr.model.Role;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
