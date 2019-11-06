42cd7eba68c6cc3ee9d8a27e356ebc6e71950747
Tomputer <tomek.gromadzki.sk@gmail.com>
2019-11-06 14:42:24 +0100

    Index implementation and sec extras

--- src/main/java/pl/gromadzki/spittr/config/SecurityConfigure.java
+++ src/main/java/pl/gromadzki/spittr/config/SecurityConfigure.java
@@ -1,29 +1,28 @@
 package pl.gromadzki.spittr.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class SecurityConfigure extends WebSecurityConfigurerAdapter {
-    @Bean
-    public UserDetailsService userDetailsService(){
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("user")
-                .roles("USER")
-                .build();
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("admin")
-                .roles("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user,admin);
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
+                auth
+                        .inMemoryAuthentication()
+                        .withUser("user")
+                        .password(passwordEncoder().encode("user"))
+                        .roles("USER")
+                        .and()
+                        .withUser("admin")
+                        .password(passwordEncoder().encode("admin"))
+                        .roles("ADMIN");
+
     }
 
     @Override
@@ -38,7 +37,13 @@ public class SecurityConfigure extends WebSecurityConfigurerAdapter {
                 .formLogin().permitAll()
                 .and()
                 .logout().permitAll()
+                .logoutSuccessUrl("/")
                 .and()
                 .csrf().disable();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
